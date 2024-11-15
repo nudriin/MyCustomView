e64aec41c4c4050d2c92a264de6261792358ae7f
Nurdin <mrsunnysummer@gmail.com>
2024-11-15 13:46:37 +0700

    style: complete my button class and insert it into activity main

--- app/src/main/java/com/nudriin/mycustomview/MyButton.kt
+++ app/src/main/java/com/nudriin/mycustomview/MyButton.kt
@@ -2,15 +2,31 @@ package com.nudriin.mycustomview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.Gravity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 
 class MyButton : AppCompatButton {
+    private var txtColor: Int = 0
+    private var enableBackground: Drawable
+    private var disableBackground: Drawable
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
+    init {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enableBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disableBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
+        background = if(isEnabled) enableBackground else disableBackground
+        setTextColor(textColors)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = if(isEnabled) "Submit" else "Isi Dulu"
     }
 }
