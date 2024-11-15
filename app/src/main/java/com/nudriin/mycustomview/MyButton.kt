bdb831a84dd3389ef39aad3829f43b2f7c90134a
Nurdin <mrsunnysummer@gmail.com>
2024-11-15 13:22:42 +0700

    initial: create custom button class

---
+++ app/src/main/java/com/nudriin/mycustomview/MyButton.kt
@@ -0,0 +1,16 @@
+package com.nudriin.mycustomview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class MyButton : AppCompatButton {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+    }
+}
